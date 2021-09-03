712f0120d4b7bb1c65a366b31d6b985be19aaaf3
Gilda <gmrp22@hotmail.com>
2021-09-02 19:00:35 -0600

    Validators

--- db/migrate/20210903003932_create_books.rb
+++ db/migrate/20210903003932_create_books.rb
@@ -1,7 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
+      t.string :name, null: false
+      t.date :pubdate, null: true
       t.timestamps
     end
   end
--- db/migrate/20210903004016_create_authors.rb
+++ db/migrate/20210903004016_create_authors.rb
@@ -1,7 +1,9 @@
 class CreateAuthors < ActiveRecord::Migration[6.1]
   def change
     create_table :authors do |t|
-
+      t.string :name, null: false
+      t.string :lastname, null: false
+      t.string :blog, null: true
       t.timestamps
     end
   end
